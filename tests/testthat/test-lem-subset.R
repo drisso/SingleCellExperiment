b4e24f27f3233461853cc3d2254fd94f0ef1dce3
Aaron Lun <alun@wehi.edu.au>
2018-04-26 17:57:47 +0100

    Added tests for non-integer LEM indexing.

--- tests/testthat/test-lem-subset.R
+++ tests/testthat/test-lem-subset.R
@@ -1,4 +1,4 @@
-# Checks the combining methods.
+# Checks the subsetting methods.
 # library(SingleCellExperiment); library(testthat); source("test-lem-subset.R")
 
 set.seed(1000)
@@ -9,32 +9,46 @@ loadings <- matrix(runif(10000), ncol=10)
 fdata <- DataFrame(WHEE=sample(LETTERS, ncol(factors)))
 lem <- LinearEmbeddingMatrix(factors, loadings, fdata)
 
-test_that("subsetting works correctly", {
-    # By row.
-    shuffled <- sample(nrow(lem))
-    lem.alt <- lem[shuffled,]
+test_that("subsetting works correctly for different index types", {
+    for (x in 1:2) {
+        if (x==1) {
+            by.row <- sample(nrow(factors), nrow(factors)/2)
+            by.col <- sample(ncol(factors), ncol(factors)/2)
+        } else if (x==2) {
+            by.row <- rbinom(nrow(factors), 1, 0.5)==1
+            by.col <- rbinom(ncol(factors), 1, 0.5)==1
+        } else {
+            colnames(lem) <- paste0("Factor_", seq_len(ncol(factors)))
+            rownames(lem) <- paste0("Cell_", seq_len(nrow(factors)))
+            dimnames(factors) <- dimnames(lem)
+            colnames(loadings) <- rownames(fdata) <- colnames(lem)
 
-    expect_identical(sampleFactors(lem.alt), factors[shuffled,])
-    expect_identical(featureLoadings(lem.alt), loadings)
-    expect_identical(factorData(lem.alt), fdata)
+            by.row <- sample(rownames(lem), nrow(factors)/2)
+            by.col <- sample(colnames(lem), ncol(factors)/2)
 
-    # By column.
-    shuffled <- sample(ncol(factors))
-    lem.alt <- lem[,shuffled]
+        }
 
-    expect_identical(sampleFactors(lem.alt), factors[,shuffled])
-    expect_identical(featureLoadings(lem.alt), loadings[,shuffled])
-    expect_identical(factorData(lem.alt), fdata[shuffled,,drop=FALSE])
+        # By row.
+        lem.alt <- lem[by.row,]
+        expect_identical(sampleFactors(lem.alt), factors[by.row,])
+        expect_identical(featureLoadings(lem.alt), loadings)
+        expect_identical(factorData(lem.alt), fdata)
+    
+        # By column.
+        lem.alt <- lem[,by.col]
+        expect_identical(sampleFactors(lem.alt), factors[,by.col])
+        expect_identical(featureLoadings(lem.alt), loadings[,by.col])
+        expect_identical(factorData(lem.alt), fdata[by.col,,drop=FALSE])
+    
+        # By row and column.
+        lem.alt <- lem[by.row, by.col]
+        expect_identical(sampleFactors(lem.alt), factors[by.row,by.col])
+        expect_identical(featureLoadings(lem.alt), loadings[,by.col])
+        expect_identical(factorData(lem.alt), fdata[by.col,,drop=FALSE])
+    }
+})
 
-    # By row and column.
-    by.row <- sample(nrow(factors), nrow(factors)/2)
-    by.col <- sample(ncol(factors), ncol(factors)/2)
-    lem.alt <- lem[by.row, by.col]
-
-    expect_identical(sampleFactors(lem.alt), factors[by.row,by.col])
-    expect_identical(featureLoadings(lem.alt), loadings[,by.col])
-    expect_identical(factorData(lem.alt), fdata[by.col,,drop=FALSE])
-
+test_that("subsetting works correctly with drop=TRUE", {
     # By row, with and without drop.
     keeper <- lem[1,]
     expect_identical(keeper, factors[1,])
@@ -58,56 +72,69 @@ test_that("subsetting works correctly", {
 })
 
 test_that("subsetting assignment works correctly", {
-     # By row.
-    src <- sample(nrow(lem), 10)
-    dest <- sample(nrow(lem), 10)
+    for (x in 1:2) {
+        if (x==1) {
+            dest.row <- sample(nrow(factors), nrow(factors)/2)
+            src.row <- sample(nrow(factors), nrow(factors)/2)
+            dest.col <- sample(ncol(factors), ncol(factors)/2)
+            src.col <- sample(ncol(factors), ncol(factors)/2)
+        } else if (x==2) {
+            dest.row <- rbinom(nrow(factors), 1, 0.5)==1
+            src.row <- sample(dest.row)
+            dest.col <- rbinom(ncol(factors), 1, 0.5)==1
+            src.col <- sample(dest.col)
+        } else {
+            colnames(lem) <- paste0("Factor_", seq_len(ncol(factors)))
+            rownames(lem) <- paste0("Gene_", seq_len(nrow(factors)))
+            dimnames(factors) <- dimnames(lem)
+            colnames(loadings) <- rownames(fdata) <- colnames(lem)
 
-    lem.alt <- lem
-    lem.alt[dest,] <- lem[src,]
+            dest.row <- sample(rownames(lem), nrow(factors)/2)
+            src.row <- sample(rownames(lem), nrow(factors)/2)
+            dest.col <- sample(colnames(lem), ncol(factors)/2)
+            src.col <- sample(colnames(lem), ncol(factors)/2)
+        }
 
-    ref <- factors
-    ref[dest,] <- ref[src,]
-    expect_identical(sampleFactors(lem.alt), ref)
-    expect_identical(featureLoadings(lem.alt), loadings)
-    expect_identical(factorData(lem.alt), fdata)
-
-    # By column.
-    src <- sample(ncol(lem), 5)
-    dest <- sample(ncol(lem), 5)
-
-    lem.alt <- lem
-    lem.alt[,dest] <- lem[,src]
-
-    ref_sf <- factors
-    ref_sf[,dest] <- ref_sf[,src]
-    expect_identical(sampleFactors(lem.alt), ref_sf)
-
-    ref_fl <- loadings
-    ref_fl[,dest] <- ref_fl[,src]
-    expect_identical(featureLoadings(lem.alt), ref_fl)
-
-    ref_fd <- fdata
-    ref_fd[dest,] <- fdata[src,]
-    expect_identical(factorData(lem.alt), ref_fd)
-
-    # By row and column.
-    dest.row <- sample(nrow(factors), nrow(factors)/2)
-    src.row <- sample(nrow(factors), nrow(factors)/2)
-    dest.col <- sample(ncol(factors), ncol(factors)/2)
-    src.col <- sample(ncol(factors), ncol(factors)/2)
+        # By row.
+        lem.alt <- lem
+        lem.alt[dest.row,] <- lem[src.row,]
     
-    lem.alt <- lem
-    lem.alt[dest.row,dest.col] <- lem[src.row,src.col]
-
-    ref_sf <- factors
-    ref_sf[dest.row,dest.col] <- ref_sf[src.row,src.col]
-    expect_identical(sampleFactors(lem.alt), ref_sf)
-
-    ref_fl <- loadings
-    ref_fl[,dest.col] <- ref_fl[,src.col]
-    expect_identical(featureLoadings(lem.alt), ref_fl)
-
-    ref_fd <- fdata
-    ref_fd[dest.col,] <- fdata[src.col,]
-    expect_identical(factorData(lem.alt), ref_fd)
+        ref <- factors
+        ref[dest.row,] <- ref[src.row,]
+        expect_identical(sampleFactors(lem.alt), ref)
+        expect_identical(featureLoadings(lem.alt), loadings)
+        expect_identical(factorData(lem.alt), fdata)
+    
+        # By column.
+        lem.alt <- lem
+        lem.alt[,dest.col] <- lem[,src.col]
+    
+        ref_sf <- factors
+        ref_sf[,dest.col] <- ref_sf[,src.col]
+        expect_identical(sampleFactors(lem.alt), ref_sf)
+    
+        ref_fl <- loadings
+        ref_fl[,dest.col] <- ref_fl[,src.col]
+        expect_identical(featureLoadings(lem.alt), ref_fl)
+    
+        ref_fd <- fdata
+        ref_fd[dest.col,] <- fdata[src.col,]
+        expect_identical(factorData(lem.alt), ref_fd)
+    
+        # By row and column.
+        lem.alt <- lem
+        lem.alt[dest.row,dest.col] <- lem[src.row,src.col]
+    
+        ref_sf <- factors
+        ref_sf[dest.row,dest.col] <- ref_sf[src.row,src.col]
+        expect_identical(sampleFactors(lem.alt), ref_sf)
+    
+        ref_fl <- loadings
+        ref_fl[,dest.col] <- ref_fl[,src.col]
+        expect_identical(featureLoadings(lem.alt), ref_fl)
+    
+        ref_fd <- fdata
+        ref_fd[dest.col,] <- fdata[src.col,]
+        expect_identical(factorData(lem.alt), ref_fd)
+    }
 })
