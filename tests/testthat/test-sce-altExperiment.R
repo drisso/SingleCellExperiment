d11284777e4201eeda928c6d0eeb1fe6fe67f134
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-14 11:22:40 -0700

    Added tests for altExperiment getters and setters.

---
+++ tests/testthat/test-sce-altExperiment.R
@@ -0,0 +1,111 @@
+# Tests for proper functioning of the altExperiment methods.
+# library(SingleCellExperiment); library(testthat); source("setup.R"); source("test-sce-altExperiment.R")
+
+sce <- loaded
+
+test_that("altExperiment getters work correctly", {
+    expect_identical(altExperiment(sce), se1)
+    expect_identical(altExperiment(sce, 2), se2)
+    expect_identical(altExperiment(sce, "Protein"), se2)
+    expect_error(altExperiment(empty), "no alternative experiments")
+
+    expect_identical(altExperimentNames(sce), c("Spike", "Protein"))
+
+    expect_identical(altAssay(sce), assay(se1))
+    expect_identical(altAssay(sce, i=2), assay(se1, 2))
+    expect_identical(altAssay(sce, "Protein"), assay(se2))
+    expect_identical(altAssay(sce, 2, 2), assay(se2, 2))
+
+    expect_identical(altAssayNames(sce), c("counts", "logcounts"))
+    expect_identical(altAssayNames(sce, 2), c("counts", "logcounts"))
+    expect_identical(altAssayNames(sce, "Protein"), c("counts", "logcounts"))
+           
+    expect_identical(altRowData(sce), rowData(se1))
+    expect_identical(altRowData(sce, 2), rowData(se2))
+    expect_identical(altRowData(sce, "Protein"), rowData(se2))
+
+    expect_identical(altRowNames(sce), rownames(se1))
+    expect_identical(altRowNames(sce, 2), rownames(se2))
+    expect_identical(altRowNames(sce, "Protein"), rownames(se2))
+})
+
+test_that("altExperiment setters work correctly", {
+    se3 <- se2
+    assay(se3) <- assay(se3) + 1
+    altExperiment(sce) <- se3
+
+    expect_identical(altExperiment(sce), se3)
+    expect_identical(altExperiment(sce, 1), se3)
+    expect_identical(altExperiment(sce, "Spike"), se3)
+
+    altExperiment(sce, 2) <- se3
+    expect_identical(altExperiment(sce, 2), se3)
+    expect_identical(altExperiment(sce, "Protein"), se3)
+
+    altExperiment(sce, 1) <- NULL
+    expect_identical(altExperimentNames(sce), "Protein")
+    altExperiment(sce, 1) <- NULL
+    expect_identical(altExperimentNames(sce), character(0))
+
+    expect_error(altExperiment(sce, 5) <- se3, "out of bounds")
+})
+
+test_that("altExperimentNames setters work correctly", {
+    altExperimentNames(sce) <- c("A", "B")
+    expect_identical(altExperimentNames(sce), c("A", "B"))
+
+    expect_error(altExperimentNames(empty) <- c("A", "B"), "no alternative experiments")
+    expect_error(altExperimentNames(sce) <- LETTERS, "more column names")
+})
+
+test_that("altAssay setters work correctly", {
+    altAssay(sce) <- assay(se1) + 1
+    expect_identical(altAssay(sce), assay(se1) + 1)
+    altAssay(sce, 2) <- assay(se2) + 2
+    expect_identical(altAssay(sce, 2), assay(se2) + 2)
+    altAssay(sce, "Protein") <- assay(se2) + 3
+    expect_identical(altAssay(sce, "Protein"), assay(se2) + 3)
+
+    altAssay(sce, i=2) <- assay(se1, i=2) + 1
+    expect_identical(altAssay(sce, i=2), assay(se1, 2) + 1)
+    altAssay(sce, "Protein", i=2) <- assay(se2, i=2) + 1
+    expect_identical(altAssay(sce, "Protein", i=2), assay(se2, 2) + 1)
+})
+
+test_that("altAssayNames setters work correctly", {
+    altAssayNames(sce) <- c("Whee", "Boo")
+    expect_identical(altAssayNames(sce), c("Whee", "Boo"))
+    altAssayNames(sce, 2) <- c("Whee", "Boo")
+    expect_identical(altAssayNames(sce, 2), c("Whee", "Boo"))
+})
+
+test_that("altRowData setters work correctly", {
+    stuff <- paste0("X", altRowData(sce)$stuff)
+    altRowData(sce)$stuff <- stuff
+    expect_identical(altRowData(sce)$stuff, stuff)
+
+    blah <- paste0("X", altRowData(sce, 2)$blah)
+    altRowData(sce, 2)$blah <- blah
+    expect_identical(altRowData(sce, 2)$blah, blah)
+
+    blah2 <- paste0("Z", altRowData(sce, 2)$blah)
+    altRowData(sce, "Protein")$blah <- blah2
+    expect_identical(altRowData(sce, "Protein")$blah, blah2)
+})
+
+test_that("altRowNames setters work correctly", {
+    rn <- altRowNames(sce) 
+    rn <- paste0("X", rn)
+    altRowNames(sce) <- rn
+    expect_identical(altRowNames(sce), rn)
+
+    rn <- altRowNames(sce, 2) 
+    rn <- paste0("Y", rn)
+    altRowNames(sce, 2) <- rn
+    expect_identical(altRowNames(sce, 2), rn)
+
+    rn <- altRowNames(sce, "Protein")
+    rn <- paste0("Z", rn)
+    altRowNames(sce, "Protein") <- rn
+    expect_identical(altRowNames(sce, 2), rn)
+})
