3ad44b00337849d8445c823053061ebc16cc6b43
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-13 18:53:43 -0700

    Added an easy splitting function to populate alternatives.

---
+++ R/splitSCEByAlt.R
@@ -0,0 +1,13 @@
+#' @export
+splitSCEByAlt <- function(x, f, ref=NULL) {
+    by.feat <- split(seq_along(f), f)
+    if (is.null(ref)) {
+        ref <- names(by.feat)[which.max(lengths(by.feat))]
+    }
+
+    x0 <- x[by.feat[[ref]],]
+    for (other in setdiff(names(by.feat), ref)) {
+        altExperiment(x0, other) <- x[by.feat[[other]],]
+    }
+    x0
+}
