80a6c1c9914c30aa0961947d99d7e61c9c370c9c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-14 17:15:26 -0700

    Fixes for updateObject method.

--- R/updateObject.R
+++ R/updateObject.R
@@ -1,9 +1,16 @@
+#' @export
 #' @importFrom BiocGenerics updateObject
 #' @importFrom S4Vectors DataFrame
 setMethod("updateObject", "SingleCellExperiment", function(object, ..., verbose=FALSE) {
-    if (objectVersion(object) < "1.5.3") {
+    if (objectVersion(object) < "1.7.1") {
         stuff <- object@reducedDims                        
         int_colData(object)$reducedDims <- do.call(DataFrame, lapply(stuff, I))
+
+        if (verbose) {
+            message("[updateObject] ", class(object), " object uses ", 
+                "internal representation\n", "[updateObject] from SingleCellExperiment ", 
+                objectVersion(object), ". ", "Updating it ... ", appendLF = FALSE)
+        }
     }
     object
 })
