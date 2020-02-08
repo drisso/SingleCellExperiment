7c2c437f3b9ca38d5a446c622ebcb62ab5f5036f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-07 21:27:20 -0800

    Removed defunct sizeFactors functions, added auto-updaters.

---
+++ R/sizeFactors.R
@@ -0,0 +1,59 @@
+#' Size factor methods
+#' 
+#' Gets or sets the size factors for all cells.
+#' 
+#' @param object A \linkS4class{SingleCellExperiment} object.
+#' @param value A numeric vector of length equal to \code{ncol(object)}, containing size factors for all cells.
+#' 
+#' @details
+#' A size factor is a scaling factor used to divide the raw counts of a particular cell to obtain normalized expression values,
+#' thus allowing downstream comparisons between cells that are not affected by differences in library size or total RNA content.
+#' The \code{sizeFactors} methods can be used to get or set size factors for all cells in a SingleCellExperiment object.
+#'
+#' When setting size factors, the values are stored in the \code{\link{colData}} as the \code{\link{sizeFactors}} field.
+#' This name is chosen for general consistency with other packages (e.g., \pkg{DESeq2})
+#' and to allow the size factors to be easily extracted from the \code{\link{colData}} for use as covariates.
+#' 
+#' @return
+#' For \code{sizeFactors}, a numeric vector is returned containing size factors for all cells.
+#' 
+#' For \code{sizeFactors<-}, a modified \code{object} is returned with size factors in its \code{\link{colData}}.
+#' 
+#' @seealso
+#' \linkS4class{SingleCellExperiment}, for the underlying class definition. 
+#'
+#' \code{librarySizeFactors} from the \pkg{scater} package
+#' or \code{computeSumFactors} from the \pkg{scran} package,
+#' as examples of functions that compute the size factors.
+#' 
+#' @author
+#' Aaron Lun
+#' 
+#' @examples
+#' example(SingleCellExperiment, echo=FALSE) # Using the class example
+#' sizeFactors(sce) <- runif(ncol(sce))
+#' sizeFactors(sce)
+#'
+#' @name SCE-sizeFactors
+NULL
+
+.sf_field <- "sizeFactor"
+
+#' @export
+#' @rdname SCE-sizeFactors
+#' @importFrom BiocGenerics sizeFactors
+setMethod("sizeFactors", "SingleCellExperiment", function(object) {
+    object <- updateObject(object)
+    output <- colData(object)[[.sf_field]]
+    names(output) <- colnames(object)
+    output
+})
+
+#' @export
+#' @rdname SCE-sizeFactors
+#' @importFrom BiocGenerics sizeFactors<-
+setReplaceMethod("sizeFactors", "SingleCellExperiment", function(object, ..., value) {
+    object <- updateObject(object)
+    colData(object)[[.sf_field]] <- value
+    object
+})
