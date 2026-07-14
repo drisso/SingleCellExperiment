0143a66b14eca11572c9479dea8fe8dab949dbd9
Giulio Benedetti <giuliobene2000@gmail.com>
2026-07-14 18:48:43 +0300

    Prevent swapAltExp from downgrading TreeSE to SCE

--- R/swapAltExp.R
+++ R/swapAltExp.R
@@ -8,7 +8,8 @@
 #' If \code{NULL}, the original is not saved.
 #' @param withColData Logical scalar specifying whether the column metadata of \code{x} should be preserved in the output.
 #'
-#' @return A SingleCellExperiment derived from \code{altExp(x, name)}.
+#' @return An object derived from \code{altExp(x, name)} and upgraded to
+#' \linkS4class{SingleCellExperiment} if not already a subclass thereof.
 #' This contains all alternative Experiments in \code{altExps(x)}, excluding the one that was promoted to the main Experiment.
 #' An additional alternative Experiment containing \code{x} may be included if \code{saved} is specified.
 #' 
@@ -54,7 +55,10 @@
 #' @export
 swapAltExp <- function(x, name, saved=mainExpName(x), withColData=TRUE) {
     y <- altExp(x, name, withColData=withColData)
-    y <- as(y, "SingleCellExperiment")
+    
+    if (!inherits(y, "SingleCellExperiment")) {
+        y <- as(y, "SingleCellExperiment")
+    }
 
     all.ae <- altExps(x, withColData=FALSE)
     altExps(y) <- all.ae[setdiff(names(all.ae), name)]
